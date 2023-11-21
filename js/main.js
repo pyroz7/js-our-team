8ed91ef75d43f289c295bd806c43d2c9bb8dd11a
pyroz7 <vincipyroz7@gmail.com>
2023-11-21 17:18:34 +0100

    half2

--- js/main.js
+++ js/main.js
@@ -1,52 +1,61 @@
 let teams = [
 {
-    name:"Wayne Barnett",
+    name:"Wayne ",
+    surname:"Barret",
     role:"Founder e CEO",
     image:"img/wayne-barnett-founder-ceo.jpg"
 },
 {
-    name:"Angela Caroll",
+    name:"Angela",
+    surname:"Carrol",
     role:"Chief Editor",
     image:"img/angela-caroll-chief-editor.jpg"
 },
 {
-    name:"Walter Gordon",
+    name:"Walter ",
+    surname:"Gordon",
     role:"Office Manager",
     image:"img/walter-gordon-office-manager.jpg"
 },
 {
-    name:"Angela Lopez",
+    name:"Angela",
+    surname:"Lopez",
     role:"Social Media Manager",
     image:"img/angela-lopez-social-media-manager.jpg"
 },
 {
-    name:"Scott Estrada",
+    name:"Scott",
+    surname:"Estrada",
     role:"Developer",
     image:"img/scott-estrada-developer.jpg"
 },
 {
-    name:"Barbara Ramos",
+    name:"Barbar",
+    surname:"Ramos",
     role:"Graphic Designer",
     image:"img/barbara-ramos-graphic-designer.jpg"
-},
+}
 
-]
+];
 
 //STAMPIAMO SU CONSOLE LE INFORMAZIONI
+
 printArrayTeams(teams);
+console.log(teams);
+
 
 
 
 
 //FUNZIONE
 
-
 function printArrayTeams(arrayTeams) {
-    for (let i = 0; i < arrayTeamslength; i++) {
+    for (let i = 0; i < arrayTeams.length; i++) {
 
-        let teams = arrayStudents[i];
+        let teams = arrayTeams[i];
 
         console.log("Il membro n° " + (i + 1) + " è:");
-        printObject(teams);
+        printArrayTeams(teams);
     }
 }
+
